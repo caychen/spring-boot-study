a62a4f375aa603da0e7ce807e50250c4d63a6c80
caychen <412425870@qq.com>
2019-12-24 20:27:17 +0800

    删除无用包

--- spring-boot-annotation-properties/src/main/java/org/com/cay/springboot/properties/DruidProperties.java
+++ spring-boot-annotation-properties/src/main/java/org/com/cay/springboot/properties/DruidProperties.java
@@ -1,10 +1,7 @@
 package org.com.cay.springboot.properties;
 
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 /**
